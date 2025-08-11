fbda30e490813b3f2e8f7174412239842c92708d
teresess <sem.tatarinov@icloud.com>
2025-08-11 23:16:33 +0500

    Error
    New files directory

---
+++ src/test/java/upd/dev/authskeleton/AuthSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package upd.dev.authskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
